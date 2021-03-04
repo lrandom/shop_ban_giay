6a123fcf6f32627107e229716383266ae54b3abf
root <luann4099@gmail.com>
2021-03-04 19:20:38 +0700

    add models + repo user

---
+++ src/main/java/edu/niit/luan/shop_ban_giay/models/User.java
@@ -0,0 +1,23 @@
+package edu.niit.luan.shop_ban_giay.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name="full_name")
+    private String fullname;
+
+    @Column(name="password")
+    private String password;
+}
